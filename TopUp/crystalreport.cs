955bd0853c44c0255863fe39e9e4bdeeeba32ba8
Thomas19SA1247 <96850385+Thomas19SA1247@users.noreply.github.com>
2022-01-12 23:37:09 +0700

    Update crystalreport.cs

--- TopUp/crystalreport.cs
+++ TopUp/crystalreport.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TopUp
 {
@@ -16,10 +17,42 @@ namespace TopUp
         {
             InitializeComponent();
         }
-
+        string connectionString = @"Data Source=LAPTOP-KQ9Q6C8H;Initial Catalog=Topup;Integrated Security=True";
         private void crystalreport_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void dgvreport_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void btnprint_Click(object sender, EventArgs e)
+        {
+            DataTable table = null;
+            string query = "select * from TransaksiTab";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(this.connectionString))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                    {
+                        table = new DataTable();
+                        adapter.Fill(table);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //handle caught exception
+            }
+
+            if (table != null)
+            {
+                dgvreport.DataSource = table.DefaultView;
+            }
         }
     }
 }
